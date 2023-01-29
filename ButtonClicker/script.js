1d6d6203930ca240c886ea608d1faf7d5fde7210
AWinn13 <anthonymichaelwinn@gmail.com>
2023-01-28 19:36:10 -0500

    ButtonClicker

---
+++ ButtonClicker/script.js
@@ -0,0 +1,12 @@
+function logout(element){
+    element.innerText = "Logout";
+}
+
+function like(element) {
+    alert("Ninja was liked");
+}
+
+function definition(element){
+    element.remove();
+
+}

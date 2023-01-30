09c833b0e7468d32eccb29e21f692c3fde0c46aa
AWinn13 <anthonymichaelwinn@gmail.com>
2023-01-30 16:55:34 -0500

    linkespt2

--- Likes/likespt2.js
+++ Likes/likespt2.js
@@ -1,19 +1,4 @@
-var count = 3;
-var count2 = 4;
-var count3 = 5;
-
-function add1(){
-    count++;
-    document.querySelector("#likes").innerText = count + " Likes"
+function add1(likesid){
+    document.querySelector(likesid).innerText++
+    
 }
-
-function add2(){
-    count2++;
-    document.querySelector("#likes2").innerText = count2 + " Likes"
-}
-
-function add3(){
-    count3++;
-    document.querySelector("#likes3").innerText = count3 + " Likes"
-}
-

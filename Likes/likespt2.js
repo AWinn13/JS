7029d28b015c3274401b64aceabdebcd558d5319
AWinn13 <anthonymichaelwinn@gmail.com>
2023-01-30 10:52:02 -0500

    Likes

---
+++ Likes/likespt2.js
@@ -0,0 +1,19 @@
+var count = 3;
+var count2 = 4;
+var count3 = 5;
+
+function add1(){
+    count++;
+    document.querySelector("#likes").innerText = count + " Likes"
+}
+
+function add2(){
+    count2++;
+    document.querySelector("#likes2").innerText = count + " Likes"
+}
+
+function add3(){
+    count3++;
+    document.querySelector("#likes3").innerText = count + " Likes"
+}
+
